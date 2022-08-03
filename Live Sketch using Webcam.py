315b4a2acfc57af3f051709c3f88d7cc8f6441a6
Tushar Tiwary <tushartiwary848@gmail.com>
2022-08-03 13:17:27 +0530

    feat: added file

---
+++ Live Sketch using Webcam.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import cv2
+import numpy as np
+
+
+# In[ ]:
+
+
+def sketch(image):
+    # Convert image to grayscale
+    img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    # Clean up image using Guassian Blur
+    img_gray_blur = cv2.GaussianBlur(img_gray, (5,5), 0)
+    
+    # Extract edges
+    canny_edges = cv2.Canny(img_gray_blur, 10, 70)
+    
+    # Do an invert binarize the image 
+    ret, mask = cv2.threshold(canny_edges, 70, 255, cv2.THRESH_BINARY_INV)
+    return mask
+
+
+# In[ ]:
+
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    cv2.imshow('Our Live Sketcher', sketch(frame))
+    if cv2.waitKey(1) == 13: #13 is the Enter Key
+        break
+
+
+# In[ ]:
+
+
+cap.release()
+cv2.destroyAllWindows()
+
